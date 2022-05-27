f138fae7bc1d50d778ea2e2474d6edfdd3f72943
Márk Nikházy <mark.nikhazy@noffz.com>
2022-05-27 08:31:30 +0200

    Add project files.

---
+++ TestWinforms/BoolAlgebra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWinforms
+{
+    class BoolAlgebra
+    {
+        private static string Converter(string input)
+        {
+            string result = input.Replace("∧", "AND").Replace("¬", "NOT ").Replace("∨", "OR").Replace("⇒", "IMPLIES");
+            return result;
+        }
+
+        public static void CheckClipboard()
+        {
+            if (Clipboard.ContainsText(TextDataFormat.Text))
+            {
+                string clipboardText = Clipboard.GetText(TextDataFormat.UnicodeText);
+                Clipboard.SetText(BoolAlgebra.Converter(clipboardText));
+            }
+        }
+    }
+}
---
+++ TestWinforms/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestWinforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            BoolAlgebra.CheckClipboard();
+        }
+    }
+}
